fe610bfe3f52571483b06709d0728eca0ff18af5
OnClickListener2048 <55997027@qq.com>
2026-07-29 23:42:05 +0800

    test: cover markdown document persistence

--- feature-chat/build.gradle.kts
+++ feature-chat/build.gradle.kts
@@ -16,6 +16,9 @@ kotlin {
             implementation(project(":feature-files"))
             implementation(project(":feature-user"))
         }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
     }
 }
 
